38406c893ccf4e308d0323dae3bd025f69fa1170
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-19 21:24:58 -0300

    Create screen and buttons for click X or O
    Programming the envent click in all buttons

--- TicTacToe/View/Screen.xaml.cs
+++ TicTacToe/View/Screen.xaml.cs
@@ -1,9 +1,48 @@
 namespace TicTacToe.View;
 
-public partial class Screen		: ContentPage
+public partial class Screen : ContentPage
 {
+	private int _counter = 0;
 	public Screen()
 	{
 		InitializeComponent();
+	}
+
+	#region Button_Clicked
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Button btn = (Button)sender;
+
+			if (_counter % 2 == 0)
+			{
+				btn.Text = "X";
+				_counter++;
+				btn.IsEnabled = false;
+			}
+			else
+			{
+				btn.Text = "O";
+				_counter++;
+				btn.IsEnabled = false;
+			}
+
+			if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
+			{
+				DisplayAlert("Vencedor".ToUpper(), "O jogador X ganhou!", "OK");
+			}
+		}
+		catch (Exception ex)
+		{
+
+			throw;
+		}
+    }
+    #endregion
+
+	private void Zerar()
+	{
+
 	}
 }
