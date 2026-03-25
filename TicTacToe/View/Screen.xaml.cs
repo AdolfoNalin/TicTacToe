208a08b0c59f49955f92622ef11cf21ecf3c3bcd
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-25 11:26:14 -0300

    End project

--- TicTacToe/View/Screen.xaml.cs
+++ TicTacToe/View/Screen.xaml.cs
@@ -2,47 +2,111 @@ namespace TicTacToe.View;
 
 public partial class Screen : ContentPage
 {
-	private int _counter = 0;
+    string[,] _board = new string[4, 3];
+    private List<Button> _listButton = new List<Button>();
+
 	public Screen()
 	{
 		InitializeComponent();
 	}
 
+    string value = "X";
 	#region Button_Clicked
 	private void Button_Clicked(object sender, EventArgs e)
 	{
 		try
 		{
-			Button btn = (Button)sender;
+            var button = (Button)sender;
+            _listButton.Add(button);
 
-			if (_counter % 2 == 0)
-			{
-				btn.Text = "X";
-				_counter++;
-				btn.IsEnabled = false;
-			}
-			else
-			{
-				btn.Text = "O";
-				_counter++;
-				btn.IsEnabled = false;
-			}
+            string[] pos = button.CommandParameter.ToString().Split(",");
 
-			if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
-			{
-				DisplayAlert("Vencedor".ToUpper(), "O jogador X ganhou!", "OK");
-			}
-		}
+            int.TryParse(pos[0].ToString(), out int line);
+            int.TryParse(pos[1].ToString(), out int column);
+
+            // Marca no tabuleiro
+            _board[line, column] = value;
+
+            // Atualiza UI
+            button.Text = value;
+            button.IsEnabled = false;
+
+            // Verifica vencedor
+            if (VerificationResut())
+            {
+                DisplayAlert("Fim de jogo", $"Jogador {value} venceu!", "OK");
+                return;
+            }
+
+            // Troca jogador
+            value = value == "X" ? "O" : "X";
+
+        }
 		catch (Exception ex)
 		{
-
+            DisplayAlert("Error", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "OK");
 			throw;
 		}
     }
     #endregion
 
-	private void Zerar()
+	private void Clear()
 	{
+		foreach(Button b in  _listButton)
+        {
+            b.Text = String.Empty;
+            b.IsEnabled = true;
+        }
+	}
 
+	private bool VerificationResut()
+	{
+		try
+		{
+            bool result = false;
+
+            string j = value;
+
+            // Linhas
+            for (int i = 0; i <= 3; i++)
+            {
+                if (_board[i, 0] == j &&
+                    _board[i, 1] == j &&
+                    _board[i, 2] == j)
+                    result = true;
+            }
+
+            // Colunas
+            for (int i = 1; i < 3; i++)
+            {
+                if (_board[1, i] == j &&
+                    _board[2, i] == j &&
+                    _board[3, i] == j)
+                    result = true;
+            }
+
+            // Diagonal principal
+            if (_board[1, 0] == j &&
+                _board[2, 1] == j &&
+                _board[3, 2] == j)
+                result = true;
+
+            // Diagonal secundária
+            if (_board[3, 2] == j &&
+                _board[2, 1] == j &&
+                _board[1, 0] == j)
+                result = true;
+
+            if (result)
+                 Clear();
+
+            return result;
+            
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Error", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "OK");
+            return false;
+		}
 	}
 }
